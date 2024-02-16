9ca62abf769abef7b64d65e0bd167047528565c5
Abolfazl Mohammadi <101124457+abolfazmz81@users.noreply.github.com>
2024-02-16 17:54:02 +0330

    Adding FSM code

---
+++ FSM/a_fsm_tb.vhd
@@ -0,0 +1,123 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   09:05:55 12/04/2023
+-- Design Name:   
+-- Module Name:   C:/Documents and Settings/string_finder/a_fsm_tb.vhd
+-- Project Name:  string_finder
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: string_finder
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.numeric_std.ALL;
+
+ENTITY a_fsm_tb IS
+END a_fsm_tb;
+
+ARCHITECTURE test OF a_fsm_tb IS
+    COMPONENT string_finder
+        PORT (
+            x : IN STD_LOGIC;
+            clk : IN STD_LOGIC;
+            reset : IN std_logic;
+            w : OUT STD_LOGIC;
+            counter : INOUT INTEGER := 0
+        );
+    END COMPONENT;
+
+    SIGNAL x : STD_LOGIC;
+    SIGNAL clk : STD_LOGIC;
+    SIGNAL w : STD_LOGIC;
+    SIGNAL counter : INTEGER := 0;
+    Signal reset : std_logic;
+
+    -- Clock period definitions
+    CONSTANT clk_period : TIME := 20 ps;
+BEGIN
+    uut : string_finder PORT MAP(
+        x => x,
+        clk => clk,
+        w => w,
+        counter => counter,
+        reset => reset
+    );
+
+    clk_process : PROCESS
+    BEGIN
+        clk <= '0';
+        WAIT FOR clk_period/2;
+        clk <= '1';
+        WAIT FOR clk_period/2;
+    END PROCESS;
+
+    stim_proc : PROCESS
+    BEGIN
+        reset <= '1';
+        w
+        wait for clk_period;
+        reset <= '0';
+        x <= '1';
+        WAIT FOR clk_period;
+        x <= '0';
+        WAIT FOR clk_period;
+        x <= '1';
+        WAIT FOR clk_period;
+        x <= '0';
+        WAIT FOR clk_period;
+        x <= '0';
+        WAIT FOR clk_period;
+        x <= '0';
+        WAIT FOR clk_period;
+        x <= '1';
+        WAIT FOR clk_period;
+        x <= '0';
+        WAIT FOR clk_period;
+        x <= '0';
+        WAIT FOR clk_period;
+        x <= '1';
+        WAIT FOR clk_period;
+        x <= '1';
+        WAIT FOR clk_period;
+        x <= '1';
+        WAIT FOR clk_period;
+        x <= '0';
+        WAIT FOR clk_period;
+        x <= '0';
+        WAIT FOR clk_period;
+        x <= '1';
+        WAIT FOR clk_period;
+        x <= '1';
+        WAIT FOR clk_period;
+        x <= '0';
+        WAIT FOR clk_period;
+        x <= '0';
+        WAIT FOR clk_period;
+        x <= '1';
+        WAIT FOR clk_period;
+        x <= '1';
+        WAIT FOR clk_period;
+        x <= '1';
+        WAIT FOR clk_period;
+
+        ASSERT false REPORT "End of simulation";
+        WAIT;
+    END PROCESS;
+END test;
---
+++ FSM/string_finder.vhd
@@ -0,0 +1,104 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    22:07:28 12/02/2023 
+-- Design Name: 
+-- Module Name:    string_finder - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity string_finder is
+    Port ( x : in  STD_LOGIC;
+           clk : in  STD_LOGIC;
+           reset : in std_logic;
+           w : out  STD_LOGIC;
+           counter : inout integer := 0);
+end string_finder;
+
+architecture Behavioral of string_finder is
+ SUBTYPE states IS STD_LOGIC_VECTOR (4 DOWNTO 0);
+    CONSTANT s0 : states := "00001";
+    CONSTANT s1 : states := "00010";
+    CONSTANT s2 : states := "00100";
+    CONSTANT s3 : states := "01000";
+    CONSTANT s4 : states := "10000";
+signal current_state : states:=s0;
+begin
+    process(clk)
+    --variable counter : integer:=0;
+    begin
+        if(rising_edge(clk))then
+            if(reset = '1')then
+                current_state <= s0;
+            else
+                      if(current_state = s0)then
+                if(x='0')then
+                    current_state<=s1;
+                    w <= '0';
+                else
+                    current_state<=s0;
+                    w <= '0';
+                end if;
+            elsif(current_state = s1)then
+                if(x='0')then
+                    current_state<=s2;
+                    w <= '0';
+                else
+                    current_state<=s0;
+                    w <= '0';
+                end if;
+            elsif(current_state = s2)then
+                if(x='0')then
+                    current_state<=s2;
+                    w <= '0';
+                else
+                    current_state<=s3;
+                    w <= '0';
+                end if;
+            elsif(current_state = s3)then
+                if(x='0')then
+                    current_state<=s1;
+                    w <= '0';
+                else
+                    current_state<=s4;
+                    w <= '1';
+                    counter <= 1+counter;
+                end if;
+            elsif(current_state = s4)then
+                if(x='0')then
+                    current_state<=s1;
+                    w <= '0';
+                else
+                    current_state<=s0;
+                    w <= '0';
+                end if;
+            end if;
+
+            end if;
+        end if;
+
+    end process;
+end Behavioral;
+
